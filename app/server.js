9da7acb9c528eb40d1ca99b53076b6a4c698bc5e
light3739 <51126304+light3739@users.noreply.github.com>
2023-07-11 23:19:23 +0200

    Fixing

--- app/server.js
+++ app/server.js
@@ -11,6 +11,15 @@ const counter = new prometheus.Counter({
   help: 'Total number of requests to the Node.js app',
 });
 
+// Define a gauge metric for whether the server is alive
+const aliveGauge = new prometheus.Gauge({
+  name: 'nodejsapp_alive',
+  help: 'Whether the Node.js app is alive',
+});
+
+// Set the initial value of the alive gauge to 1
+aliveGauge.set(1);
+
 app.get('/', function (req, res) {
   // Increment the counter metric for each request to the app
   counter.inc();
@@ -24,8 +33,8 @@ app.get('/profile-picture-andrea', function (req, res) {
       console.log(err);
       res.status(500).send("Error reading file");
     } else {
-      res.writeHead(200, {'Content-Type': 'image/jpg' });
-      res.end(data);
+      res.set('Content-Type', 'image/jpg');
+      res.send(data);
     }
   });
 });
@@ -33,8 +42,8 @@ app.get('/profile-picture-andrea', function (req, res) {
 app.get('/profile-picture-ari', function (req, res) {
   try {
     let img = fs.readFileSync(path.join(__dirname, "images/profile-ari.jpeg"));
-    res.writeHead(200, {'Content-Type': 'image/jpg' });
-    res.end(img, 'binary');
+    res.set('Content-Type', 'image/jpg');
+    res.send(img);
   } catch (error) {
     console.log(error);
     res.status(500).send("Error reading file");
@@ -42,11 +51,25 @@ app.get('/profile-picture-ari', function (req, res) {
 });
 
 app.get('/metrics', function (req, res) {
+  // Set the value of the alive gauge to 1
+  aliveGauge.set(1);
+
   // Expose the Prometheus metrics endpoint
   res.set('Content-Type', prometheus.register.contentType);
-  res.end(prometheus.register.metrics());
+  res.send(prometheus.register.metrics());
 });
 
-app.listen(8080, function () {
-  console.log("App listening on port 3000!");
+// Start the server and set the value of the alive gauge to 1
+const server = app.listen(8080, function () {
+  console.log("App listening on port 8080!");
+  aliveGauge.set(1);
+});
+
+// Set the value of the alive gauge to 0 when the server is closed
+process.on('SIGTERM', function () {
+  server.close(function () {
+    console.log("Server closed");
+    aliveGauge.set(0);
+    process.exit(0);
+  });
 });
