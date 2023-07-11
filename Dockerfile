6fda43a626aa599781ad7edb8557fa440baac083
light3739 <51126304+light3739@users.noreply.github.com>
2023-07-11 20:03:43 +0200

    Updating to fix problems with express Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node:13-alpine
 WORKDIR /usr/app
 COPY app ./
+RUN npm install express
 EXPOSE 3000
 CMD [ "node", "server.js" ]
--- app/server.test.js
+++ app/server.test.js
@@ -2,9 +2,6 @@ const path = require('path');
 const fs = require('fs')
 
 test('main index.html file exists', () => {
-  const filePath = path.join(__dirname, "index.html")
-  expect(fs.existsSync(filePath)).toBeTruthy();
+    const filePath = path.join(__dirname, "index.html")
+    expect(fs.existsSync(filePath)).toBeTruthy();
 });
-// test('1==0', () => {       //failure test
-//   expect(1===0, true);
-// });
